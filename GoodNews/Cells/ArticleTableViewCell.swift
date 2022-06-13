5278366223db0b7c192ae57c0ea93c85f6918de2
yemretekin <emretekin08@gmail.com>
2022-06-13 18:40:15 +0300

    project is done

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by Emre Tekin on 13.06.2022.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell{
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
--- GoodNews/Controllers/NewsListTableViewController.swift
+++ GoodNews/Controllers/NewsListTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import Foundation
 
 class NewsListTableViewController: UITableViewController {
+    
+    private var articleListVM: ArticleListViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,13 +18,37 @@ class NewsListTableViewController: UITableViewController {
         setup()
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM.numberOfRowsInSection(section)
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else{
+            fatalError("ArticleTableViewCell not Founnd")
+        }
+        let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.description
+        return cell
+    }
     private func setup(){
         
         self.navigationController?.navigationBar.prefersLargeTitles = true
         
         let url = URL(string: "https://newsapi.org/v2/everything?q=bitcoin&apiKey=b75d6d5e58c54d8bb187da900a0880ca")!
         
-        WebService().getArticles(url: url){ _ in
+        WebService().getArticles(url: url){ articles in
+            
+            if let articles = articles {
+                self.articleListVM = ArticleListViewModel(articles: articles)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
             
         }
     }
